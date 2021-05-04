9aac2ec5c28c6ba6e395e3bd40220a72fc4e3ea6
Neel Patel <neelp2021@gmail.com>
2021-05-04 15:53:53 -0700

    updated with java swing lib

--- src/com/neelp/EmailApp.java
+++ src/com/neelp/EmailApp.java
@@ -8,18 +8,38 @@ Github profile: https://github.com/neel-p1103
 package com.neelp;
 
 import javax.swing.*;
+import java.sql.Array;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class EmailApp {
 
     public static void main(String[] args) {
 
-        String firstName = JOptionPane.showInputDialog("Enter first name: ");
-        String lastName = JOptionPane.showInputDialog("Enter last name: ");
+        Email email;
+        Employees employees;
+        ArrayList<Email> empArr = new ArrayList<Email>();
 
-        Email email1 = new Email(firstName, lastName);
-        email1.setAltEmail("example@example.com");
-        JOptionPane.showMessageDialog(null,"Alternate email: "+email1.getAlternateEmail());
-        JOptionPane.showMessageDialog(null,email1.display());
+        int numEmployees = Integer.parseInt(JOptionPane.showInputDialog(null, "How many employees"
+                + " would you like to add?"));
+
+        for(int i = 0; i < numEmployees; i++){
+
+            String firstName = JOptionPane.showInputDialog("Enter first name: ");
+            String lastName = JOptionPane.showInputDialog("Enter last name: ");
+
+            email = new Email(firstName, lastName);
+            empArr.add(email);
+
+            email.setAltEmail("example@example.com");
+            //JOptionPane.showMessageDialog(null,"Alternate email: "+email.getAlternateEmail());
+
+            JOptionPane.showMessageDialog(null,email.display());
+        }
+
+        employees = new Employees(empArr);
+        employees.getEmpArr();
+
+
     }
 }
